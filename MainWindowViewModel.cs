409f18aeda5822773d1ec790a8d76437f9b77ece
MemeMachineSO <meme>
2023-02-27 19:33:02 -0600

    Add project files.

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using TreeViewProblem.ViewModels;
+
+namespace TreeViewProblem
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        public ObservableCollection<KeyViewModel> Keys { get; set; } = new();
+
+        public ICommand TreeViewItemExpanded
+            => new RelayCommand(() => MessageBox.Show(""));
+
+        public MainWindowViewModel()
+        {
+            Keys.Add(new KeyViewModel("Item1"));
+            Keys.Add(new KeyViewModel("Item2"));
+            Keys.Add(new KeyViewModel("Item3"));
+            Keys.Add(new KeyViewModel("Item4"));
+            Keys.Add(new KeyViewModel("Item5"));
+        }
+    }
+}
---
+++ ViewModels/KeyViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeViewProblem.ViewModels
+{
+    public class KeyViewModel
+    {
+        List<string> values = new List<string>();
+        public List<KeyViewModel> SubKeys { get; } = new();
+
+        public string name { get; }
+
+        public KeyViewModel(string keyName)
+        {
+            name = keyName;
+
+            if (!name.Contains("test"))
+                SubKeys.Add(new KeyViewModel("test"));
+            else if (name == "test")
+                SubKeys.Add(new KeyViewModel("test2"));
+        }
+    }
+}
